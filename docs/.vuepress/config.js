84232a7731678af8ee925739c934fed79de7eda1
Ted Morin <morinted@gmail.com>
2022-01-19 16:31:17 -0500

    Add favicon and theme logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,41 @@ module.exports = {
   title: 'Art of Chording',
   description:
     'Teach yourself stenography for free. Use steno with Plover to type faster and more ergonomically than on a keyboard.',
+  head: [
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+    ],
+    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+    [
+      'link',
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+    ],
+    ['meta', { name: 'msapplication-TileColor', content: '#da532c' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+  ],
   plugins: [
     ['@vuepress/google-analytics', { ga: 'UA-102613911-3' }],
     [
@@ -17,6 +52,7 @@ module.exports = {
   themeConfig: {
     lastUpdated: 'Last Updated',
     repo: 'morinted/art_of_chording',
+    logo: '/android-chrome-192x192.png',
     docsDir: 'docs',
     activeHeaderLinks: false,
     editLinks: true,
